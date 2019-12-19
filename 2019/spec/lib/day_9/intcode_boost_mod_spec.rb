c16f63b8b3b77b2b7340d3cb34144506d3450e51
sam_l <samuel.lee@juul.com>
2019-12-19 11:38:09 -0800

    update day 9 spec

--- 2019/spec/lib/day_9/intcode_boost_mod_spec.rb
+++ 2019/spec/lib/day_9/intcode_boost_mod_spec.rb
@@ -1,15 +1,14 @@
-require 'day_9/intcode_boost_mod'
+require 'day_9/intcode_mod_boost'
 require 'spec_helper'
 
-RSpec.describe IntcodeMod do
-  let(:instance) { described_class.new(num_ary: num_ary) }
+RSpec.describe IntcodeModBoost do
+  let(:instance) { described_class.new(num_ary: num_ary, input_codes: [1]) }
   let(:num_ary) {
     [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99]
   }
   describe '#diag_code' do
     it 'should return correct result' do
-      instance.call
-      expect(instance.diag_code).to eq num_ary
+      expect(instance.call).to eq num_ary
     end
   end
 end
